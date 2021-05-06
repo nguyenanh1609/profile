d267acd36d74fc94348be8e57768768839ac1034
nguyenanh1609 <nguyenhuuanh102000@gmail.com>
2021-05-06 13:42:30 +0700

    done

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function btnbar() {
+    document.getElementById('menu-sub').style.transform="translateX(0%)"
+    document.getElementById("click-clear").style.display="block"
+    document.getElementById("menu-bar").style.display="none"
+
+}
+function btnclose() {
+    document.getElementById('menu-sub').style.transform="translateX(110%)"
+    document.getElementById("click-clear").style.display="none"
+    document.getElementById("menu-bar").style.display="block"
+
+}
